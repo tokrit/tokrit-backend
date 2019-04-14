ae9ff978ce586c709deca049c5603ec845846dd2
HyunJung <hjung1017@gmail.com>
2019-04-14 18:26:44 -0400

    Update schema

--- db-schema-gen.sql
+++ db-schema-gen.sql
@@ -1,12 +1,20 @@
 --DROP TABLE accounts; -- Uncomment to reset db
 
-CREATE TABLE IF NOT EXISTS accounts (
+CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     email VARCHAR(255),
-    encryptedPassword VARCHAR(255),
-    salt VARCHAR(255),
-    firstName VARCHAR(30),
-    lastName VARCHAR(30),
-    created BIGINT,
-    isAdmin BOOLEAN
+    profile_picture_url VARCHAR(255),
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    created_at DATE,
+    user_role VARCHAR(30)
+);
+
+CREATE TABLE IF NOT EXISTS announcements (
+  id SERIAL PRIMARY KEY,
+  author INT REFERENCES users(id),
+  title VARCHAR(128),
+  content VARCHAR(4000),
+  created_at DATE,
+  modified_at DATE
 );
